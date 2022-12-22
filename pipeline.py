35551319ba47bb73da891f126eac8727eb2b9b27
Pierlou <pierlou.ramade@data.gouv.fr>
2022-12-22 12:11:25 +0100

    feat: add code_parent and libelle_geo to stats table

--- pipeline.py
+++ pipeline.py
@@ -6,31 +6,30 @@ from unidecode import unidecode
 import swifter
 from datetime import date
 
+to_keep = [
+    'id_mutation',
+    'date_mutation',
+    'code_departement',
+    'code_commune',
+    'id_parcelle',
+    'nature_mutation',
+    'code_type_local',
+    'type_local',
+    'valeur_fonciere',
+    'surface_reelle_bati'
+]
+epci= pd.read_csv('epci.csv', sep=',', encoding= 'utf8', dtype=str)
 export = pd.DataFrame(None)
 for year in range(date.today().year-5, date.today().year+1):
-    df_ = pd.read_csv(f'full_{year}.csv', sep=',', encoding= 'utf8',
-                     dtype={"code_commune" : str,
-                            "code_postal" : str,
-                            "code_departement" : str,
-                            "ancien_nom_commune" : str,
-                            "code_nature_culture_speciale" : str,
-                            "nature_culture_speciale" : str,},
-    #                nrows=500000
-                   )
-    epci= pd.read_csv('epci.csv', sep=',', encoding= 'utf8', dtype=str)
-    to_keep = [
-        'id_mutation',
-        'date_mutation',
-        'code_departement',
-        'code_commune',
-        'id_parcelle',
-        'nature_mutation',
-        'code_type_local',
-        'type_local',
-        'valeur_fonciere',
-        'surface_reelle_bati'
-    ]
-    df = df_[to_keep]
+    df_ = pd.read_csv(
+        f'full_{year}.csv', 
+        sep=',', 
+        encoding= 'utf8',
+        dtype={"code_commune" : str,
+               "code_departement" : str,},
+        usecols=to_keep,
+        )
+    df = df_.drop_duplicates()
     df = pd.merge(df, epci, on='code_commune', how='left')
     df['code_section'] = df['id_parcelle'].str[:10]
     df = df.drop('id_parcelle', axis=1)
@@ -52,24 +51,25 @@ for year in range(date.today().year-5, date.today().year+1):
     ventes['month'] = ventes['date_mutation'].swifter.apply(lambda x: int(x.split('-')[1]))
     print(len(ventes))
 
-    ## drop mutations multitypes pour les prix au m², impossible de classr une mutation qui contient X maisons et Y appartements par exemple
+    ## drop mutations multitypes pour les prix au m², impossible de classer une mutation qui contient X maisons et Y appartements par exemple
     multitypes = ventes[['id_mutation', 'code_type_local']].value_counts()
-    multitypes = multitypes.loc[multitypes>1].unstack()
-    mutations_drop = multitypes.loc[sum([multitypes[c].isna() for c in multitypes.columns])<len(multitypes.columns)-1].index
-    ventes_nodup = ventes.loc[~ventes['id_mutation'].isin(mutations_drop)]
+    multitypes_ = multitypes.unstack()
+    mutations_drop = multitypes_.loc[sum([multitypes_[c].isna() for c in multitypes_.columns])<len(multitypes_.columns)-1].index
+    ventes_nodup = ventes.loc[~(ventes['id_mutation'].isin(mutations_drop)) & ~(ventes['code_type_local'].isna())]
     print(len(ventes_nodup))
 
     ## group par mutation, on va chercher la surface totale de la mutation pour le prix au m²
     surfaces = ventes_nodup.groupby(['id_mutation'])['surface_reelle_bati'].sum().reset_index()
     surfaces.columns = ['id_mutation', 'surface_totale_mutation']
+    ## avec le inner merge sur surfaces on se garantit aucune ambiguïté sur les type_local
     ventes_nodup = ventes.drop_duplicates(subset = 'id_mutation')
+    ventes_nodup = pd.merge(ventes_nodup, surfaces, on='id_mutation', how='inner')
     print(len(ventes_nodup))
-    ventes_nodup = pd.merge(ventes_nodup, surfaces, on='id_mutation')
     
     ## pour une mutation donnée la valeur foncière est globale, on la divise par la surface totale, sachant qu'on n'a gardé que les mutations monotypes
     ventes_nodup['prix_m2'] = ventes_nodup['valeur_fonciere']/ventes_nodup['surface_totale_mutation']
     ventes_nodup['prix_m2'] = ventes_nodup['prix_m2'].replace([np.inf, -np.inf], np.nan)
-    
+
     ## pas de prix ou pas de surface
     ventes_nodup = ventes_nodup.dropna(subset=['prix_m2'])
 
@@ -85,11 +85,12 @@ for year in range(date.today().year-5, date.today().year+1):
             nb_.columns = ['nb_ventes_'+unidecode(c.split(' ')[0].lower()) if c != f'code_{echelle}' else c for c in nb_.columns]
             
             ## pour mean et median on utlise le df nodup, dans lequel on a drop_dup sur les mutations
-            mean = ventes_nodup.loc[ventes_nodup['code_type_local'].isin(types_of_interest)].groupby([f'code_{echelle}', 'month', 'type_local'])['prix_m2'].mean()
+            grouped = ventes_nodup.loc[ventes_nodup['code_type_local'].isin(types_of_interest)].groupby([f'code_{echelle}', 'month', 'type_local'])['prix_m2']
+            mean = grouped.mean()
             mean_ = mean.loc[mean.index.get_level_values(1)==m].unstack().reset_index().drop('month', axis=1)
             mean_.columns = ['moy_prix_m2_'+unidecode(c.split(' ')[0].lower()) if c != f'code_{echelle}' else c for c in mean_.columns]
 
-            median = ventes_nodup.loc[ventes_nodup['code_type_local'].isin(types_of_interest)].groupby([f'code_{echelle}', 'month', 'type_local'])['prix_m2'].median()
+            median = grouped.median()
             median_ = median.loc[median.index.get_level_values(1)==m].unstack().reset_index().drop('month', axis=1)
             median_.columns = ['med_prix_m2_'+unidecode(c.split(' ')[0].lower()) if c != f'code_{echelle}' else c for c in median_.columns]
 
@@ -113,5 +114,27 @@ for year in range(date.today().year-5, date.today().year+1):
         all_month['annee_mois'] = f'{year}-{"0"+str(m) if m<10 else m}'
         export = pd.concat([export, all_month])
     print("Done with", year)
+
+departements = pd.read_csv('departement_2022.csv', dtype=str, usecols=['DEP', 'LIBELLE'])
+departements = departements.rename({'DEP':'code_geo', 'LIBELLE': 'libelle_geo'}, axis=1)
+departements['code_parent'] = 'all'
+epci_communes = epci[['code_commune', 'code_epci']]
+epci['code_parent'] = epci['code_commune'].apply(lambda code: code[:2] if code[:2]!='97' else code[:3])
+epci = epci.drop('code_commune', axis=1)
+epci = epci.drop_duplicates(subset=['code_epci', 'code_parent']).rename({'code_epci': 'code_geo'}, axis=1)
+communes = pd.read_csv('commune_2022.csv', dtype=str, usecols=['TYPECOM', 'COM', 'LIBELLE'])
+communes = communes.loc[communes['TYPECOM'].isin(['COM', 'ARM'])].rename({'COM':'code_geo', 'LIBELLE': 'libelle_geo'}, axis=1).drop('TYPECOM', axis=1)
+epci_communes = epci_communes.rename({'code_commune': 'code_geo', 'code_epci': 'code_parent'}, axis=1)
+communes = pd.merge(communes, epci_communes, on='code_geo', how='outer')
+libelles_parents = pd.concat([departements, epci, communes])
+libelles_parents['libelle_geo'] = libelles_parents['libelle_geo'].apply(unidecode)
+
+export = pd.merge(export, libelles_parents, on='code_geo', how='left')
+export.loc[export['echelle_geo']=='section', 'code_parent'] = export.loc[export['echelle_geo']=='section']['code_geo'].str.slice(0,5)
+export.loc[export['echelle_geo']=='nation', 'code_parent'] = "-"
+export.loc[(export['echelle_geo']=='commune') & (export['code_geo'].str.startswith('75')), 'code_parent'] = '200054781'
+export.loc[(export['echelle_geo']=='commune') & (export['code_geo'].str.startswith('13')), 'code_parent'] = '200054807'
+export.loc[(export['echelle_geo']=='commune') & (export['code_geo'].str.startswith('69')), 'code_parent'] = '200046977'
+export['code_parent'] = export['code_parent'].fillna('NA')
     
 export.to_csv('files/stats_dvf.csv', sep=',', encoding='utf8', index=False)
