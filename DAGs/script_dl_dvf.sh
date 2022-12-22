62697e191ea58d5bae91b9d7faac46f03095520f
Pierlou <pierlou.ramade@data.gouv.fr>
2022-12-22 12:10:40 +0100

    feat: dowload departements and communes

--- DAGs/script_dl_dvf.sh
+++ DAGs/script_dl_dvf.sh
@@ -5,17 +5,19 @@ DATADIR="/tmp/data"
 rm -rf $DATADIR
 mkdir -p $DATADIR
 
+echo temp > $DATADIR/query.txt
+
+URL_departements='https://www.insee.fr/fr/statistiques/fichier/6051727/departement_2022.csv'
+URL_communes='https://www.insee.fr/fr/statistiques/fichier/6051727/commune_2022.csv'
+
+curl $URL_departements > $DATADIR/departements.csv
+curl $URL_communes > $DATADIR/communes.csv
+
 for YEAR in `seq $five_ago $curr_year`
 do
   echo $YEAR && [ ! -f $DATADIR/full_$YEAR.csv.gz ] && wget -r -np -nH -N --cut-dirs 5  https://files.data.gouv.fr/geo-dvf/latest/csv/$YEAR/full.csv.gz -O $DATADIR/full_$YEAR.csv.gz
 done
 
-# for YEAR in `seq $five_ago $curr_year`
-# do
-#   echo $YEAR && [ ! -f $DATADIR/full_$YEAR.csv.gz ] && curl  https://files.data.gouv.fr/geo-dvf/latest/csv/$YEAR/full.csv.gz > $DATADIR/full_$YEAR.csv.gz
-# done
-
 find $DATADIR -name '*.gz' -exec gunzip -f '{}' \;
 
 cd $DATADIR && ls
-# export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
